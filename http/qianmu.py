e20256e8ffe807c2b9051bfe1bb9abf455f4220c
guye <guye999@gmail.com>
2017-11-16 12:20:49 +0800

    fix sup issue

--- http/qianmu.py
+++ http/qianmu.py
@@ -1,59 +1,79 @@
 # coding:utf-8
 
 """
-爬取大学排名列表并保存为JSON文件，另建立一个尽可能齐全的大学资料库，用JSON文件保存
+爬取大学排名列表并print出来，另建立一个尽可能齐全的大学资料库，也print出来
 """
 import string
 import lxml.etree
 import requests
+from w3lib.html import remove_tags
 
 
 domain = '140.143.192.76:8002'
 start_url = 'http://140.143.192.76:8002/2018USNEWS世界大学排名'
-link_queue = []
+link_queue = [] # 保存等待抓取的url
 
 
 def fetch(url):
+    """使用request抓取页面"""
     r = requests.get(url)
     r.raise_for_status()
     return r.text
 
 
 def clean(s):
+    """去除字符串两边的空格"""
     return s.strip() if s else ''
 
-def x(ele):
-    if not ele:
-        return ''
-    return ele[0].strip()
+
+def filter(html):
+    """过滤网页源码中的特殊符号和sup标签"""
+    return remove_tags(html, which_ones=('sup',)).replace('\n', '')\
+    .replace('\r', '').replace('\t', '')
+
 
 def parse(html):
+    """解析入口页面信息"""
+    # 使用全局变量
     global link_queue
     dom = lxml.etree.HTML(html)
-    rows = dom.xpath("//div[@id='content']//tr")
+    # 获取除第一行以外的所有行
+    rows = dom.xpath("//div[@id='content']//tr[position()>1]")
     for row in rows:
+        # 提取每行的文本，并用clean函数进行处理
         columns = map(clean, row.xpath('./td//text()'))
         for column in columns:
             print(column, end=',')
         print()
+        #提取每行第二个单元格中的超链接
         link = row.xpath('./td[2]//@href')
         if link:
+            # 放入待抓取列表
             link_queue += link
 
 
 def parse_university(html):
-    dom = lxml.etree.HTML(html)
+    """解析大学详情页面的信息"""
+    dom = lxml.etree.HTML(filter(html))
+    # 先获取一个父节点，以减少重复代码
     wiki_content = dom.xpath('//div[@id="wikiContent"]')[0]
+    # 获取大学名称
     title = wiki_content.xpath('./h1[@class="wikiTitle"]/text()')[0]
+    # 获取左边的列
     keys = wiki_content.xpath('./div[@class="infobox"]/table//tr/td[1]/p/text()')
-    values = wiki_content.xpath('./div[@class="infobox"]/table//tr/td[2]/p/text()')
+    # 获取右边的每一个单元格
+    cols = dom.xpath('//*[@id="wikiContent"]/div[@class="infobox"]/table//tr/td[2]')
+    # 因为有的单元格中存在多行，所以用逗号连接起来
+    values = [','.join(col.xpath('.//text()')) for col in cols]
+    # 用title作为key，存储成字典
     info = {title: dict(zip(keys, values))}
     print(info)
 
 
 if __name__ == '__main__':
-    content = fetch(start_url)
-    parse(content)
+    # 处理入口页面，并提取数据
+    parse(fetch(start_url))
+    # 反转List以便先进先出
     link_queue.reverse()
     while link_queue:
         link =link_queue.pop()
--- http/qianmu_thread.py
+++ http/qianmu_thread.py
@@ -1,68 +1,119 @@
 # coding:utf-8
-
-"""
-爬取大学排名列表并保存为JSON文件，另建立一个尽可能齐全的大学资料库，用JSON文件保存
-"""
+import time
 import threading
 from queue import Queue
-import lxml.etree
 import requests
+import lxml.etree
+from w3lib.html import remove_tags
 
 
-start_url = 'http://140.143.192.76:8002/2018USNEWS世界大学排名'
-q = Queue()
-threads = []
-downloader_num = 20
+START_URL = 'http://140.143.192.76:8002/2018USNEWS世界大学排名'
+link_queue = Queue()    # 队列，保存等抓取的url
+DOWNLOADER_NUM = 10   # 启动的线程数量
+threads = []    # 线程列表，保存Thread对象
+download_pages = 0
 
 
-def fetch(url):
-    r = requests.get(url)
-    r.raise_for_status()
-    return r.text
+def fetch(url, raise_err=True):
+    global download_pages
+    try:
+        # 使用requests抓取url
+        r = requests.get(url)
+        download_pages += 1
+        return r.text
+    except Exception as e:
+        # 如果报错则print出来
+        print(e)
+    else:
+        # 如果没报错，则检查http的返回码是否正常
+        if raise_err:
+            r.raise_for_status()
+
+
+def filter(html):
+    """过滤网页源码中的特殊符号和sup标签"""
+    return remove_tags(html, which_ones=('sup',)).replace('\n', '')\
+    .replace('\r', '').replace('\t', '')
 
 
 def clean(s):
+    """去除字符串两边的空格"""
     return s.strip() if s else ''
 
 
-def downloader():
-    while True:
-        link = q.get()
-        if link is None:
-            break
-        parse_university(fetch(link))
-        q.task_done()
-
-
-def parse_university(html):
+def parse(html):
+    """解析入口页面信息"""
+    # 使用全局变量
+    global link_queue
     dom = lxml.etree.HTML(html)
-    wiki_content = dom.xpath('//div[@id="wikiContent"]')[0]
-    title = wiki_content.xpath('./h1[@class="wikiTitle"]/text()')[0]
-    keys = wiki_content.xpath('./div[@class="infobox"]/table//tr/td[1]/p/text()')
-    values = wiki_content.xpath('./div[@class="infobox"]/table//tr/td[2]/p/text()')
-    info = {title: dict(zip(keys, values))}
-    print(info)
-
-
-if __name__ == '__main__':
-    for i in range(downloader_num):
-        t = threading.Thread(target=downloader)
-        t.start()
-        threads.append(t)
-    print('%s threads are ready' % len(threads))
-    dom = lxml.etree.HTML(fetch(start_url))
-    rows = dom.xpath("//div[@id='content']//tr")
+    # 获取除第一行以外的所有行
+    rows = dom.xpath("//div[@id='content']//tr[position()>1]")
     for row in rows:
+        # 提取每行的文本，并用clean函数进行处理
         columns = map(clean, row.xpath('./td//text()'))
         for column in columns:
             print(column, end=',')
         print()
-        link = row.xpath('./td[2]//@href')
+        #提取每行第二个单元格中的超链接
+        link = row.xpath('./td[2]//@href')        
         if link:
-            q.put(link[0])
-    q.join()
+            link = link[0]
+            if not link.startswith('http://'):
+                link = 'http://140.143.192.76:8002/%s' % link
+            # 将url放入队列
+            link_queue.put(link)
 
-    for i in range(downloader_num):
-        q.put(None)
+
+def parse_univercity(html):
+    """解析大学详情页面的信息"""
+    dom = lxml.etree.HTML(filter(html))
+    # 先获取一个父节点，以减少重复代码
+    wiki_content = dom.xpath('//div[@id="wikiContent"]')[0]
+    # 获取大学名称
+    title = wiki_content.xpath('./h1[@class="wikiTitle"]/text()')[0]
+    # 获取左边的列
+    keys = wiki_content.xpath('./div[@class="infobox"]/table//tr/td[1]/p/text()')
+    # 获取右边的每一个单元格
+    cols = dom.xpath('//*[@id="wikiContent"]/div[@class="infobox"]/table//tr/td[2]')
+    # 因为有的单元格中存在多行，所以用逗号连接起来
+    values = [','.join(col.xpath('.//text()')) for col in cols]
+    # 用title作为key，存储成字典
+    info = {title: dict(zip(keys, values))}
+    print(info)
+
+
+
+def downloader():
+    while True:
+        # 阻塞直到从队列获得了一条消息
+        link = link_queue.get()
+        # 如果收到的消息是None，则退出循环
+        if link is None:
+            break
+        # 执行下载、解析操作
+        parse_univercity(fetch(link, raise_err=False))
+        # 向队列发送完成任务的消息
+        link_queue.task_done()
+        print('remaining queue: %s' % link_queue.qsize())
+
+if __name__ == '__main__':
+    start_time = time.time()
+    # 抓取解析入口页面
+    parse(fetch(START_URL))
+    # 启动线程
+    for i in range(DOWNLOADER_NUM):
+        t = threading.Thread(target=downloader)
+        t.start()
+        threads.append(t)
+    # 阻塞到队列清空为止
+    link_queue.join()
+
+    # 向队列发送DOWNLOADER_NUM个None，以通知线程退出
+    for i in range(DOWNLOADER_NUM):
+        link_queue.put(None)
+    # 退出线程
     for t in threads:
         t.join()
+    # 计算抓取的耗时
+    cost_seconds = time.time() - start_time
+    print('download %s pages in %.2f seconds' % (download_pages, cost_seconds))
