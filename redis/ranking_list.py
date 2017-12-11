1e4b46340f06150656a451ea2ed98d7f2fa49681
guye <guye999@gmail.com>
2017-12-11 15:49:32 +0800

    rewrite ranking_list

--- redis/ranking_list.py
+++ redis/ranking_list.py
@@ -1,33 +1,35 @@
 # coding: utf-8
-
-"""
-
-"""
-
-import sys
-import random
 import time
-from datetime import datetime, timedelta
+import random
 import redis
-from faker import Faker
+from datetime import datetime, timedelta
+import faker
 
-
-f = Faker()
 r = redis.Redis()
+faker = faker.Faker('zh_CN')
 DEADLINE = datetime.now() + timedelta(hours=1)
 
 
 def exam(course, students=50):
+    print '%s exam started...' % course
     for i in range(students):
-        name = f.name() 
+        name = faker.name()
         time_remaining = (DEADLINE - datetime.now()).total_seconds()
-        score = '%s.%s' % (random.randint(80, 100), str(time_remaining).replace('.', ''))
-        print i, course, name, score
-        r.zadd(course, name , score)
-
+        score = '%s.%s' % (random.randint(60, 100),
+                str(time_remaining).replace('.', ''))
+        r.zadd(course, name, score)
+        print('%s: %s' % (name, score))
         time.sleep(0.1)
 
+def top(course, rank_range=10):
+    stus = r.zrevrange('English', 0, 10, withscores=True)
+    for i, s in enumerate(stus):
+        print i + 1, s[0], s[1] 
 
 
 if __name__ == '__main__':
-    exam('English')
+    courses = ['English', 'Chinese', 'Math']
+    for course in courses:
+        exam(course, students=50)
+        top(course)
+
